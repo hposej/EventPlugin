336c353b77d5bc12c071de2a519767d8006d9cdc
hposej <jcr2609@email.vccs.edu>
2016-07-08 23:50:10 -0400

    First commit for plugin. Testing if the plugin will run externally.

---
+++ src/edu/vccs/email/jcr2609/event/EventPlugin.java
@@ -0,0 +1,18 @@
+package edu.vccs.email.jcr2609.event;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventPlugin extends JavaPlugin {
+	
+	@Override
+	public void onLoad() {}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {}
+	
+}
