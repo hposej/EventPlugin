16b30b0f9f6df2a3ca68b5f68a38869176e19557
hposej <jcr2609@email.vccs.edu>
2016-07-09 00:17:54 -0400

    Testing to see if this registers

--- src/edu/vccs/email/jcr2609/event/EventPlugin.java
+++ src/edu/vccs/email/jcr2609/event/EventPlugin.java
@@ -1,5 +1,6 @@
 package edu.vccs.email.jcr2609.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventPlugin extends JavaPlugin {
@@ -9,7 +10,7 @@ public class EventPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		Bukkit.broadcastMessage("Is this going to work?");
 	}
 	
 	@Override
